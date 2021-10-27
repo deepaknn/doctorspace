69fe28d056bf213b1788c3ce7fa33feaa3f39d74
deepaknn <deepaknn1518@gmail.com>
2021-10-28 00:30:24 +0530

    Initial commit

---
+++ src/main/java/app/doctorspace/doctorspace/DoctorspaceApplication.java
@@ -0,0 +1,13 @@
+package app.doctorspace.doctorspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/doctorspace/doctorspace/DoctorspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.doctorspace.doctorspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
